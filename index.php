1a8700f287aa75c2d32e6d67664f5ef608601723
hailm <hai.lemanh.1310@gmail.com>
2022-05-14 15:49:39 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,148 @@
 <?php $data["title"] = "Trang chủ"; ?>
 <?php $body = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-section-small home__video">
+    <div class="uk-container">
+        <div class="item__32">
+            <h3 class="home__title uk-h3 uk-text-center">Trải nghiệm video lớp học</h3>
+        </div>
+        <div class="item__32">
 
+        </div>
+        <div class="item__32 uk-text-center">
+            <button class="uk-button uk-button-default home__dky__box1__btn">Đăng ký nhận tư vấn</button>
+        </div>
+    </div>
+</div>
+<div class="uk-section-small">
+    <div class="uk-container">
+        <div class="item__90">
+            <div class="item__32">
+                <h3 class="home__title uk-h3 uk-text-center">Báo chí nói về Chip Chip</h3>
+            </div>
+            <div class="item__32">
+                <div class="uk-flex-center" uk-grid>
+                    <div class="uk-width-auto@m">
+                        <div class="uk-padding-small home__baochi__box">
+                            <div class="home__baochi__box__txt1">Autem consectetur sed est nisi optio ut sapiente ut ducimus. Neque enim et  tpora incidunt id minima facilis id. Suscipit non eligendi saepe earum velit nostrum. Culpa quod facere. Facere et et est itaque voluptas odio dolor et id.</div>
+                            <div class="home__baochi__box__txt2">Báo điện tử VietnamNet</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item__32">
+                <div class="uk-child-width-auto uk-flex-center" uk-grid>
+                    <?php
+                    $data = array(
+                        'images/Frame 306.png',
+                        'images/Frame 300.png',
+                        'images/Frame 305.png',
+                        'images/Frame 302.png',
+                        'images/Frame 303.png',
+                        'images/Frame 308.png',
+                        'images/Frame 307.png',
+                        'images/Frame 299.png',
+                    );
+                    foreach ($data as $v): ?>
+                    <div>
+                        <img class="home__baochi__logo" src="<?= $v ?>" alt="">
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+        <div class="item__90">
+            <div class="item__32">
+                <h3 class="home__title uk-h3 uk-text-center">Những con số ấn tượng</h3>
+            </div>
+            <div class="item__32">
+                <div class="uk-child-width-1-3@m uk-grid-small uk-grid-30-m uk-grid-match" uk-grid>
+                    <?php
+                    $data = array(
+                        array(
+                            'txt1' => '+250.000',
+                            'txt2' => 'Lớp học hoàn thành',
+                        ),
+                        array(
+                            'txt1' => '+3.000',
+                            'txt2' => 'Học sinh chính thức',
+                        ),
+                        array(
+                            'txt1' => '300',
+                            'txt2' => 'Giáo viên nước ngoài',
+                        ),
+                    );
+                    foreach ($data as $k=>$v): ?>
+                    <div class="home__consoantuong__column">
+                        <div class="home__consoantuong__box uk-padding-small uk-flex uk-flex-middle">
+                            <div class="uk-width-1-1">
+                                <div class="home__consoantuong__box__item home__consoantuong__box__txt1"><?= $v['txt1'] ?></div>
+                                <div class="home__consoantuong__box__item home__consoantuong__box__txt2"><?= $v['txt2'] ?></div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+        <div class="item__90">
+            <div class="uk-grid-collapse uk-grid-match" uk-grid>
+                <div class="uk-width-1-2@m">
+                    <div class="uk-text-center home__dky__box">
+                        <img src="images/Frame313.png" alt="">
+                    </div>
+                </div>
+                <div class="uk-width-expand">
+                    <div class="uk-padding-small home__dky__box1 uk-form-stacked uk-flex uk-flex-middle">
+                        <div class="uk-width-1-1">
+                            <div class="item__15">
+                                <div class="home__dky__box1__txt">Vui lòng điền thông tin, bộ phận tư vấn của Chip Chip sẽ liên hệ lại bạn trong thời gian sớm nhất</div>
+                            </div>
+                            <div class="item__15">
+                                <div class="item__20">
+                                    <label class="uk-form-label home__dky__box1__label" for="form-stacked-text">Tên của con</label>
+                                    <div class="uk-form-controls">
+                                        <input class="uk-input home__dky__box1__input" id="form-stacked-text" type="text" placeholder="Nhập tên của con">
+                                    </div>
+                                </div>
+                                <div class="item__20">
+                                    <label class="uk-form-label home__dky__box1__label" for="form-stacked-text">Năm sinh</label>
+                                    <div class="uk-form-controls">
+                                        <div class="uk-inline uk-width-1-1">
+                                            <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: calendar"></span>
+                                            <input class="uk-input home__dky__box1__input" id="form-stacked-text" type="text" placeholder="Nhập hoặc chọn năm sinh">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="item__20">
+                                    <label class="uk-form-label home__dky__box1__label" for="form-stacked-text">Số điện thoại</label>
+                                    <div class="uk-form-controls">
+                                        <input class="uk-input home__dky__box1__input" id="form-stacked-text" type="text" placeholder="Nhập số điện thoại liên hệ">
+                                    </div>
+                                </div>
+                                <div class="item__20">
+                                    <button class="uk-button uk-button-default home__dky__box1__btn uk-width-1-1">Đăng ký nhận tư vấn</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Footer-->
+<div class="footer uk-section-xsmall uk-background-muted">
+    <div class="uk-container">
+        <div class="uk-grid-small uk-grid-30-m" uk-grid>
+            <div class="uk-width-expand">
+                <div class="footer__txt">Hotline: 1900 066 818 | Tầng 9, N2, 210 Lê Trọng Tấn, toà nhà ACCI, 210 Lê Trọng Tấn, Thanh Xuân, Hà Nội</div>
+            </div>
+            <div class="uk-width-auto@m">
+                <div class="footer__txt">Bản quyền thuộc về Công ty cổ phần Công Nghệ Giáo Dục Chip Chip</div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/Footer-->
 <?php require "template-parts/layouts/footer.php"; ?>
